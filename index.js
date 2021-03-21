f362c134b1212d960c9c29cac67469d4dd9f4360
ProgGeddy <geddy.israel@gmail.com>
2021-03-20 18:47:16 -0600

    Add bot commands and command folder structure

--- index.js
+++ index.js
@@ -10,18 +10,16 @@ const { prefix, token } = require('./config.json');
 // Create a new Discord client
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
+const commandFolders = fs.readdirSync('./commands');
 
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-
-	// set a new item in the Collection
-	// with the key as the command name and the value as the exported module
-	client.commands.set(command.name, command);
+for (const folder of commandFolders) {
+	const commandFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const command = require(`./commands/${folder}/${file}`);
+		client.commands.set(command.name, command);
+	}
 }
 
-// When the client is ready, run this code
 // This event will only trigger one time after logging in
 client.once('ready', () => {
 	console.log('We have liftoff!');
@@ -31,12 +29,14 @@ client.on('message', message => {
 	if (!message.content.startsWith(prefix) || message.author.bot) { return; }
 
 	const args = message.content.slice(prefix.length).trim().split(/ +/);
-	const command = args.shift().toLowerCase();
+	const commandName = args.shift().toLowerCase();
 
-	if (!client.commands.has(command)) { return; }
+	if (!client.commands.has(commandName)) { return; }
+
+	const command = client.commands.get(commandName);
 
 	try {
-		client.commands.get(command).execute(message, args);
+		command.execute(message, args);
 	}
 	catch (error) {
 		console.error(error);
