59830cb75d80116c4b9b6973b2dc69d1c8145ff0
ProgGeddy <geddy.israel@gmail.com>
2021-03-20 18:22:13 -0600

    Get simple dynamic command handling pattern in place

--- index.js
+++ index.js
@@ -1,30 +1,46 @@
-// Require the discord.js module
+// Require the discord.js module.
 const Discord = require('discord.js');
+
+// fs is Node's native file system module.
+const fs = require('fs');
 
 // Retrieve any config variables necessary.
 const { prefix, token } = require('./config.json');
 
 // Create a new Discord client
 const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+
+	// set a new item in the Collection
+	// with the key as the command name and the value as the exported module
+	client.commands.set(command.name, command);
+}
 
 // When the client is ready, run this code
 // This event will only trigger one time after logging in
 client.once('ready', () => {
-	console.log('Ready!');
+	console.log('We have liftoff!');
 });
 
 client.on('message', message => {
-	if (message.content === `${prefix}ping`) {
-		message.channel.send('Pong.');
+	if (!message.content.startsWith(prefix) || message.author.bot) { return; }
+
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
+	const command = args.shift().toLowerCase();
+
+	if (!client.commands.has(command)) { return; }
+
+	try {
+		client.commands.get(command).execute(message, args);
 	}
-	else if (message.content === `${prefix}beep`) {
-		message.channel.send('Boop.');
-	}
-	else if (message.content === `${prefix}server`) {
-		message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
-	}
-	else if (message.content === `${prefix}user-info`) {
-		message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
+	catch (error) {
+		console.error(error);
+		message.reply('there was an error trying to execute that command!');
 	}
 });
 
