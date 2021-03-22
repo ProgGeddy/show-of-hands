4e325366edcf896b65ec4ab1b9db2c64af89cfb5
ProgGeddy <geddy.israel@gmail.com>
2021-03-21 22:19:20 -0600

    Add dynamic event handling and refactor index

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
-// Require the discord.js module.
 const Discord = require('discord.js');
+const client = new Discord.Client();
+const { token } = require('./config.json');
 
 // fs is Node's native file system module.
 const fs = require('fs');
 
-// Retrieve any config variables necessary.
-const { prefix, token } = require('./config.json');
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
 
-// Create a new Discord client
-const client = new Discord.Client();
+for (const file of eventFiles) {
+	const event = require(`./events/${file}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args, client));
+	}
+	else {
+		client.on(event.name, (...args) => event.execute(...args, client));
+	}
+}
 
 client.cooldowns = new Discord.Collection();
 client.commands = new Discord.Collection();
@@ -23,74 +30,6 @@ for (const folder of commandFolders) {
 		client.commands.set(command.name, command);
 	}
 }
-
-// This event will only trigger one time after logging in
-client.once('ready', () => {
-	console.log('We have liftoff!');
-});
-
-client.on('message', message => {
-	if (!message.content.startsWith(prefix) || message.author.bot) { return; }
-
-	const args = message.content.slice(prefix.length).trim().split(/ +/);
-	const commandName = args.shift().toLowerCase();
-
-	const command = client.commands.get(commandName)
-		|| client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
-
-	if (!command) { return; }
-
-	if (command.guildOnly && message.channel.type === 'dm') {
-		return message.reply('I can\'t execute that command inside DMs!');
-	}
-
-	if (command.permissions) {
-		const authorPerms = message.channel.permissionsFor(message.author);
-		if (!authorPerms || !authorPerms.has(command.permissions)) {
-			return message.reply('You can not do this!');
-		}
-	}
-
-	if (command.args && !args.length) {
-		let reply = `You didn't provide any arguments, ${message.author}!`;
-
-		if (command.usage) {
-			reply += `\nThe proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
-		}
-
-		return message.channel.send(reply);
-	}
-
-	const { cooldowns } = client;
-
-	if (!cooldowns.has(command.name)) {
-		cooldowns.set(command.name, new Discord.Collection());
-	}
-
-	const now = Date.now();
-	const timestamps = cooldowns.get(command.name);
-	const cooldownAmount = (command.cooldown || 3) * 1000;
-
-	if (timestamps.has(message.author.id)) {
-		const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
-
-		if (now < expirationTime) {
-			const timeLeft = (expirationTime - now) / 1000;
-			return message.reply(`Please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
-		}
-	}
-
-	timestamps.set(message.author.id, now);
-	setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
-
-	try {
-		command.execute(message, args);
-	}
-	catch (error) {
-		console.error(error);
-		message.reply('there was an error trying to execute that command!');
-	}
-});
 
 // Login to Discord with your app's token
 client.login(token);
